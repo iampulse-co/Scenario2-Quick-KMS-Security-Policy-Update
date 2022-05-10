3fadc12fd24804b150097c2d8b9e7fcd8bb04f34
Kyler Middleton <kymidd@gmail.com>
2022-05-10 15:59:22 -0500

    Oops, IAM is hard ;)

--- main.tf
+++ main.tf
@@ -107,6 +107,15 @@ resource "aws_kms_key" "ecr_cmk" {
       "Version" : "2012-10-17",
       "Statement" : [
         {
+          "Sid" : "Permit same-account principals",
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+          },
+          "Action" : "kms:*",
+          "Resource" : "*"
+        },
+        {
           "Sid" : "Deny Support Team",
           "Effect" : "Deny",
           "Principal" : {
